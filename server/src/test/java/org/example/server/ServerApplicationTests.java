83feefd0c3fc46ddf7306fd842c5268ddc24d875
Jeremy-Nourri <nourri.jer@gmail.com>
2025-01-13 09:42:16 +0100

    fix jwt variable env for tests

--- server/src/test/java/org/example/server/ServerApplicationTests.java
+++ server/src/test/java/org/example/server/ServerApplicationTests.java
@@ -1,5 +1,7 @@
 package org.example.server;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -7,6 +9,14 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class ServerApplicationTests {
+
+    @BeforeAll
+    static void setupEnvironment() {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET", "default-secret"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION", "3600"));
+    }
 
     @Test
     void contextLoads() {
