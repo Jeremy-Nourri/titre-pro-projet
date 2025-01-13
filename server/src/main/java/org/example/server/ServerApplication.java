e94dd8d677846d578a960c96e61dc677641b8f3a
Jeremy-Nourri <nourri.jer@gmail.com>
2025-01-13 09:17:11 +0100

    fix: variables env

--- server/src/main/java/org/example/server/ServerApplication.java
+++ server/src/main/java/org/example/server/ServerApplication.java
@@ -1,14 +1,20 @@
 package org.example.server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class ServerApplication {
-
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+
         SpringApplication.run(ServerApplication.class, args);
     }
-
 }
