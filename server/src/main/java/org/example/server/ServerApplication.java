b357a48ce9096217058d699532b8124868d39ea2
Jeremy-Nourri <nourri.jer@gmail.com>
2025-01-15 11:36:07 +0100

    fix: dotenv in class serverApplcation

--- server/src/main/java/org/example/server/ServerApplication.java
+++ server/src/main/java/org/example/server/ServerApplication.java
@@ -7,13 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
+        // Dotenv dotenv = Dotenv.configure().load();
 
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+        // System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        // System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        // System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        // System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        // System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
 
         SpringApplication.run(ServerApplication.class, args);
 
